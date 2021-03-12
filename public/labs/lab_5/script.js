d169567afa3bc16d4ce220d17c0b7dbe909627c2
Katherine Vo <katherinevo09@gmail.com>
2021-03-12 01:27:48 -0500

    added js from A1, updated with changes from leaflet tutorial

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,11 +1,45 @@
 function mapInit() {
   // follow the Leaflet Getting Started tutorial here
-  return map;
+  const mymap = L.map('mapid').setView([38.9897, -76.9378], 13);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoia3Rocm52IiwiYSI6ImNrN3RiYWF0cTBwYzYzaHB1dml0cmF6bHQifQ.r9U_UZKY2IQisQMqIci4xg'
+  }).addTo(mymap);
+  return mymap;
 }
 
 async function dataHandler(mapObjectFromFunction) {
   // use your assignment 1 data handling code here
   // and target mapObjectFromFunction to attach markers
+  const form = document.querySelector('#search-form');
+  const search = document.querySelector('#search');
+  const targetList = document.querySelector('.target-list');
+
+  const request = await fetch('/api');
+  const data = await request.json();
+
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const filtered = data.filter((record) => record.zip.includes(search.value) && record.geocoded_column_1);
+
+    filtered.forEach((item) => {
+      const longLat = item.geocoded_column_1.coordinates;
+      const marker = L.marker([longLat[1], longLat[0]]).addTo(mapObjectFromFunction);
+
+      const appendItem = document.createElement('li');
+      appendItem.classList.add('block');
+      appendItem.classList.add('list-item');
+      appendItem.innerHTML = `
+        <div class="list-header is-size-5">${item.name}</div>
+        <address class="is-size-six">${item.address_line_1}</address>`;
+      targetList.append(appendItem);
+    });
+  });
 }
 
 async function windowActions() {
